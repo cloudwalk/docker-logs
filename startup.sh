38b951e03c71af072fca2aff4c1bce8517fe0d31
allanino <allaninocencio@yahoo.com.br>
2014-07-11 23:17:19 -0300

    Rotate logs twice a day.

--- startup.sh
+++ startup.sh
@@ -3,5 +3,12 @@
 # Create dir for logrotate logs
 mkdir -p $LOG_DIR
 
-# Just to keep container alive
-/bin/bash
+# Execute logrotate regurlary (couldn't get cron to work and need some work for container):
+while true
+do
+    # Rotate logs and append output messages to $LOG_DIR/logrotate.log 
+    logrotate  -v /etc/logrotate.d/logrotate_docker.conf >> $LOG_DIR/logrotate.log 2>&1
+    
+    # Execute it twice a day
+    sleep 43200
+done 
